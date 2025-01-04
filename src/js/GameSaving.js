14baec6b5045a10069cac7548adcfdd957510c02
steklon <steklo_one@mail.ru>
2025-01-04 23:07:45 +1000

    comments removed in GameSaving

--- src/js/GameSaving.js
+++ src/js/GameSaving.js
@@ -1,13 +1,13 @@
 export default class GameSaving {
   constructor(saving) {
     const data = JSON.parse(saving);
-    this.id = data.id; // id сохранения
-    this.created = data.created; // timestamp создания
+    this.id = data.id;
+    this.created = data.created;
     this.userInfo = {
-      "id": data.userInfo.id, // user id
-      "name": data.userInfo.name, // user name
-      "level": data.userInfo.level, // user level
-      "points": data.userInfo.points // user points
+      id: data.userInfo.id,
+      name: data.userInfo.name,
+      level: data.userInfo.level,
+      points: data.userInfo.points
     };
   }
 }
